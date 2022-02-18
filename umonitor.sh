5d9aa8cb44628cdeb81d24f9341bb63e6ed7968a
Eugene Aleynikov <eugene@kamopka.home>
2022-02-18 01:19:53 -0800

    faster failure detection

--- umonitor.sh
+++ umonitor.sh
@@ -2,7 +2,7 @@
 ###
 # How often to test for connectivity
 POLLINTERVAL=20
-PKTS=2
+PKTS=1
 
 # Consider we are down only if we cannot reach any of those
 HOST1='8.8.8.8'  # Google
@@ -25,11 +25,11 @@ fi
 while : ; do
 
   POLLTIME=$(date +'%s')
-  fping -C"${PKTS}" -q "${HOST1}" > /dev/null 2>&1
+  fping -C"${PKTS}" -p120 -q "${HOST1}" > /dev/null 2>&1
   if [ "$?" -ne 0 ] ; then
-    fping -C"${PKTS}" -q "${HOST2}" > /dev/null 2>&1
+    fping -C"${PKTS}" -p120 -q "${HOST2}" > /dev/null 2>&1
     if [ "$?" -ne 0 ] ; then
-      fping -C"${PKTS}" -q "${HOST3}" > /dev/null 2>&1
+      fping -C"${PKTS}" -p120 -q "${HOST3}" > /dev/null 2>&1
       if [ "$?" -ne 0 ] ; then
 	# we are really down, now spit out mtr stats
         mtr -rn ${HOST1}
